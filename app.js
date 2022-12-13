b8a6d8605bbf6e85f0b81ac00eeae6e92ffaa9a6
dvdvip1 <dvip.d.patel@slrtce.in>
2022-12-13 22:19:41 +0530

    adding ejs

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const express= require('express');
-const app = express();
+const express = require('express');
 const bodyParser = require('body-parser');
-const port = 3000;
+const ejs = require('ejs');
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/', (req, res) => 
-    res.send('Hello World!'));
-app.listen(port, () => 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+const port = 3000;
+app.get('/', (req, res) => {
+    var days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var today = new Date();
+    var CurrentDay=today.getDay();
+   res.render("list",{kindOfDay:days[CurrentDay]});
+});
+
+app.listen(port, () =>
     console.log(`Example app listening on port ${port}!`));
