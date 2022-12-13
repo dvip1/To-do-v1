869ddb0440589d2dcc3eae950f89001b2c696b98
dvdvip1 <dvip.d.patel@slrtce.in>
2022-12-13 19:31:03 +0530

    initial files

---
+++ app.js
@@ -0,0 +1,10 @@
+const express= require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', (req, res) => 
+    res.send('Hello World!'));
+app.listen(port, () => 
+    console.log(`Example app listening on port ${port}!`));
