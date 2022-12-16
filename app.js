12f0a9bcd22d0b89f415e32cf3a7cafe76d4e96d
dvdvip1 <dvip.d.patel@slrtce.in>
2022-12-16 18:28:01 +0530

    adding date directory

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const date= require(__dirname+ "/date.js");
 const app = express();
 let items = [];
 let workItems = [];
@@ -10,14 +11,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 const port = 3000;
 app.get('/', (req, res) => {
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: 'numeric',
-        month: 'long'
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
+    let day= date;
     res.render("list", { TaskType: day, NewItem: items, dir: '/' });
 });
 app.post('/', (req, res) => {
