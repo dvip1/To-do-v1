98f2a63aa9e4548ad8fa615b112410748587618a
dvdvip1 <dvip.d.patel@slrtce.in>
2022-12-16 12:08:54 +0530

    adding post request element to li tags using ejs

--- app.js
+++ app.js
@@ -2,17 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const app = express();
+let items=["Buy Food","Cook Food","Eat Food "];
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 const port = 3000;
 app.get('/', (req, res) => {
-    var days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    var today = new Date();
-    var CurrentDay=today.getDay();
-   res.render("list",{kindOfDay:days[CurrentDay]});
-});
+    let today = new Date();
+    let options = {
+        weekday: "long",
+        day: 'numeric',
+        month: 'long'
+    };
 
-app.listen(port, () =>
-    console.log(`Example app listening on port ${port}!`));
+    let day = today.toLocaleDateString("en-US", options);
+    res.render("list", { kindOfDay: day,NewItem:items});
+});
+app.post('/',(req, res)=>{
+    let item = req.body.new_item;
+    items.push(item);
+    res.redirect("/");
+})
+
+app.listen(port, () => 
+    console.log(`App listening on port ${port}!`));
